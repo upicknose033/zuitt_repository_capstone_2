87f2bcb8942349a16f547a8705d6aeea55af7fd6
TheAngelSabathiel <jomari.angel.jacinto@gmail.com>
2025-11-18 18:10:53 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Server Dependencies and modules
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Google API Client 
+const passport = require("passport");
+const session = require("express-session");
+
+// Routes
